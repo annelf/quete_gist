065fe762425db245c17c9c7666887f2b942cf63b
Anne-Laure Fontaine <anne.lfontaine@gmail.com>
2016-09-18 19:21:04 +0200

    initialisation du dépôt

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+<div id="footer">
+	<div class="citation">
+		<p>Do not panic keep coding - Anonymous Wilder</p>
+	</div>
+</div>
+</html>
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<title>Mon CV</title>
+</head>
+
+	<body>
+		<div id="header"> <!--début header-->
+			<h1>Curriculum Vitae d'Anne-Laure Fontaine</h1>
+        </div> <!--fin header-->
+
+        <div id="container"> <!--début du header-->
+
+        	<div id="left-box"> <!--début left-box-->
+        		<img src="http://img1.imagilive.com/0916/FullSizeRenderef8.jpg" class="image" alt="ma photo" />
+        		<br />
+        		<p class="description">Les RP étaient mon quotidien, remplacés aujourd'hui par des lignes de codes, les journalistes ont laissé place aux languages informatiques, seul mon café, reste un fidèle allié qui ne cèdera pas sa place.</p>
+
+        		<ul>
+        			<li>Tel : 06 27 80 ** **</li>
+        			<li>Mail : anne.lfontaine@gmail.com</li>
+        			<li>Adresse : 9 rue de l'écu d'or - 45000 Orléans</li>
+        		</ul>
+        	
+        		<ul>
+        			<li>Français</li>
+        			<li>Anglais</li>
+        			<li>Allemand</li>
+        			<li>Italien</li>
+        		</ul>
+
+        		<ul>
+        			<li>Peinture</li>
+        			<li>Équitation</li>
+        			<li>Automobile</li>
+        		</ul>
+        	</div> <!--fin left-box-->
+
+        	<div id="right-box"> <!--début right-box-->
+
+        		<div id="experiences-box"> <!--début experiences-box-->
+
+        			<h2>Mes expériences</h2>
+
+        			<div class="experience"> <!--début experience-->
+        				<h3>Attachée de presse freelance</h3>
+        				<p>Missions intérimaires en région Centre</p>
+        			</div> <!--fin experience-->
+        			<div class="experience"> <!--début experience-->
+        				<h3>Chargée de communication</h3>
+        				<p>EVH PR Agency Sydney - Pôle Fashion and Luxury</p>
+        			</div> <!--fin experience-->
+        			<div class="experience"> <!--début experience-->
+        				<h3>Chargée de communication France et Europe</h3>
+        				<p>Make Up For Ever Paris - Département France et Europe</p>
+        			</div> <!--fin experience-->
+
+        		</div> <!--fin experiences-box-->
+
+        		<div id="studies-box"> <!--début studies-box-->
+
+        			<h2>Mes études</h2>
+
+        			<div class="study"> <!--début study-->
+        				<h3>Wild Code School</h3>
+        				<p>Formation développeur web et mobile</p>
+        			</div> <!--fin study-->
+        			<div class="study"> <!--début study-->
+        				<h3>EFAP - École Française des Attachés de Presse</h3>
+        				<p>Diplôme d'attachée de presse spécialisée dans les médias</p>
+        			</div> <!--fin study-->
+
+        		</div> <!--fin studies-box-->
+
+        	</div> <!--fin right-box-->
+
+        </div> <!--fin du container-->
+
+        <?php
+
+                include('footer.php');
+
+        ?>
+        
+	</body>
+	</html>
